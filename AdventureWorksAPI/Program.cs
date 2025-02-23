5bc6e663ab16d3ec26b5edcf141359335e6df01f
Davin Mickelson <davin@mndeveloper.com>
2025-02-23 16:47:32 -0600

    Adjusted Program.cs

--- AdventureWorksAPI/Program.cs
+++ AdventureWorksAPI/Program.cs
@@ -4,43 +4,43 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AdventureWorksAPI
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+			// Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+			builder.Services.AddControllers();
+			// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+			builder.Services.AddOpenApi();
 
-            builder.Services.AddDbContext<AdventureWorksContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDb")));
+			builder.Services.AddDbContext<AdventureWorksContext>(options =>
+					options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDb")));
 
-                  builder.Services.AddCors(OptionsBuilderConfigurationExtensions =>
-                  {
-                              OptionsBuilderConfigurationExtensions.AddPolicy("AllowAngularApp",
-                                              builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-                  });
+			builder.Services.AddCors(OptionsBuilderConfigurationExtensions =>
+			{
+				OptionsBuilderConfigurationExtensions.AddPolicy("AllowAngularApp",
+																		builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+			});
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.MapOpenApi();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseCors("AllowAngularApp");
-            app.MapControllers();
+			app.UseCors("AllowAngularApp");
+			app.MapControllers();
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
